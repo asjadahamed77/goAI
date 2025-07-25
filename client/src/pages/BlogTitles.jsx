0626da7c75bd3ea8bfa54bbac9b986da26ec247a
asjadahamed77 <ahamedasjad77@gmail.com>
2025-07-25 19:27:32 +0530

    Completed Blog Titles and Write Articles

--- client/src/pages/BlogTitles.jsx
+++ client/src/pages/BlogTitles.jsx
@@ -1,5 +1,12 @@
-import { Edit, Hash, Sparkles } from 'lucide-react';
+import { Hash, Sparkles } from 'lucide-react';
 import React, { useState } from 'react'
+import axios from "axios"
+import { useAuth } from '@clerk/clerk-react';
+import toast from 'react-hot-toast';
+import Markdown from 'react-markdown';
+
+
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL
 
 const BlogTitles = () => {
   const blogCategories = [
@@ -15,9 +22,39 @@ const BlogTitles = () => {
 
   const [selectedCategory, setSelectedCategory] = useState("General"); 
   const [input, setInput] = useState("");
+  const [loading, setLoading] = useState(false)
+  const [content, setContent] = useState("")
+
+  const {getToken} = useAuth()
 
   const onSubmitHandler = async (e) => {
     e.preventDefault();
+    try {
+      setLoading(true)
+      const prompt = `Generate a blog title for the keyword ${input} in the category ${selectedCategory}`
+
+      const {data} = await axios.post('/ai/generate-blog-title',{
+        prompt
+      },{
+        headers:{
+          Authorization: `Bearer ${await getToken()}`
+        }
+      })
+
+      if(data.success){
+        setContent(data.content)
+      }else{
+        toast.error(data.message)
+       
+        
+      }
+
+    } catch (error) {
+      toast.error(error.message)
+      console.log(error.message);
+      
+    }
+    setLoading(false)
   };
   return (
     <div className="h-full overflow-y-scroll p-6 flex items-start flex-wrap gap-4 text-slate-700">
@@ -57,7 +94,9 @@ const BlogTitles = () => {
       </div>
       <br />
       <button className="w-full bg-gradient-to-br from-primary to-secondary text-white flex justify-center items-center gap-2 px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer hover:opacity-90 duration-300 transition-all ease-in-out">
-        <Hash className="w-5" />
+      {
+            loading ? <span className="w-4 h-4 my-1 rounded-full border-2 border-t-transparent animate-spin"></span> : <Hash className="w-5" />
+          }
         Generate Title
       </button>
     </form>
@@ -67,12 +106,24 @@ const BlogTitles = () => {
         <Hash className="w-5 h-5 text-primary" />
         <h1 className="text-xl font-semibold">Generated Titles</h1>
       </div>
-      <div className="flex-1 flex justify-center items-center">
+      {
+        !content ? (
+          <div className="flex-1 flex justify-center items-center">
           <div className="text-sm flex flex-col items-center gap-5 text-gray-400">
         <Hash className="w-9" />
         <p>Enter a keyword and click "Genarate Title" to get started</p>
           </div>
       </div>
+        ): (
+          <div className='className="mt-3 h-full overflow-y-scroll text-sm text-slate-600"'>
+           <div className='reset-tw'>
+           <Markdown>
+              {content}
+            </Markdown>
+           </div>
+          </div>
+        )
+      }
     </div>
   </div>
   )
--- client/src/pages/WriteArticle.jsx
+++ client/src/pages/WriteArticle.jsx
@@ -1,5 +1,12 @@
 import { Edit, Sparkles } from "lucide-react";
 import React, { useState } from "react";
+import axios from "axios"
+import { useAuth } from "@clerk/clerk-react";
+import toast from "react-hot-toast";
+import Markdown from "react-markdown";
+
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL
+
 
 const WriteArticle = () => {
   const articleLength = [
@@ -19,9 +26,38 @@ const WriteArticle = () => {
 
   const [selectedLength, setSelectedLength] = useState(articleLength[0]);
   const [input, setInput] = useState("");
+  const [loading, setLoading] = useState(false)
+  const [content, setContent] = useState("")
+
+  const {getToken} = useAuth()
 
   const onSubmitHandler = async (e) => {
     e.preventDefault();
+    try {
+      setLoading(true)
+      const prompt = `Write an article about ${input} in ${selectedLength.text}`
+
+      const {data} = await axios.post('/ai/generate-article',{
+        prompt, length: selectedLength.length
+      },{
+        headers:{
+          Authorization: `Bearer ${await getToken()}`
+        }
+      })
+
+      if(data.success){
+        setContent(data.content)
+      }else{
+        toast.error(data.message)
+       
+        
+      }
+
+    } catch (error) {
+      toast.error(error.message)
+      console.log(error.message);
+    }
+    setLoading(false)
   };
 
   return (
@@ -61,8 +97,11 @@ const WriteArticle = () => {
           ))}
         </div>
         <br />
-        <button className="w-full bg-gradient-to-br from-primary to-secondary text-white flex justify-center items-center gap-2 px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer hover:opacity-90 duration-300 transition-all ease-in-out">
-          <Edit className="w-5" />
+        <button disabled={loading} className="w-full bg-gradient-to-br from-primary to-secondary text-white flex justify-center items-center gap-2 px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer hover:opacity-90 duration-300 transition-all ease-in-out">
+          {
+            loading ? <span className="w-4 h-4 my-1 rounded-full border-2 border-t-transparent animate-spin"></span> : <Edit className="w-5" />
+          }
+          
           Generate Article
         </button>
       </form>
@@ -72,12 +111,24 @@ const WriteArticle = () => {
           <Edit className="w-5 h-5 text-primary" />
           <h1 className="text-xl font-semibold">Generated Article</h1>
         </div>
-        <div className="flex-1 flex justify-center items-center">
-            <div className="text-sm flex flex-col items-center gap-5 text-gray-400">
-          <Edit className="w-9" />
-          <p>Enter a topic and click "Genarate Article" to get started</p>
+       {
+        !content ? (
+          <div className="flex-1 flex justify-center items-center">
+          <div className="text-sm flex flex-col items-center gap-5 text-gray-400">
+        <Edit className="w-9" />
+        <p>Enter a topic and click "Genarate Article" to get started</p>
+          </div>
+      </div>
+        ) : (
+          <div className="mt-3 h-full overflow-y-scroll text-sm text-slate-600">
+            <div className="reset-tw">
+            <Markdown>
+            {content}
+            </Markdown>
             </div>
-        </div>
+          </div>
+        )
+       }
       </div>
     </div>
   );
